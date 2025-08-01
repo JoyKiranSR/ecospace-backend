97e39f71e709efb7b3659b38b7013f428e840b5c
Joy Kiran S R <srjoykiran@gmail.com>
2025-08-02 03:10:35 +0530

    added not allowed routes for /soils

---
+++ src/api/routes/soil-route.js
@@ -0,0 +1,29 @@
+// src/api/routes/soil-route.js
+
+/**
+ * @module soil-route
+ * Ecospace Soil Routes
+ * 
+ * @description This module defines the routes for soil-related endpoints in the Ecospace backend.
+ * It includes routes for creating soil, fetching all soils, and fetching a soil by its ID.
+ * It uses the Express Router and applies validation middleware to ensure that the requests are properly formatted.
+ */
+
+// Core module imports
+const { Router } = require("express");
+
+// Initialize the router
+const routes = Router();
+
+// Define the soil routes
+routes.all("/", (req, res, next) => {
+    if (!["POST", "GET"].includes(req.method)) {
+        console.warn(`Method ${req.method} not allowed on /soils`);
+        // Handle unsupported methods
+        return res.status(405).json({ message: "Method Not Allowed" });
+    }
+    next();
+});
+
+// Export the routes for use in the main application
+module.exports = routes;

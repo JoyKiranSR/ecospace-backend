446454e34eec3a4c40e739bf8160bb6f35320c0c
Joy Kiran S R <srjoykiran@gmail.com>
2025-08-10 05:14:16 +0530

    Added validator middleware functions for update

--- src/api/middlewares/pest-type-middleware.js
+++ src/api/middlewares/pest-type-middleware.js
@@ -1,27 +1,42 @@
 // src/api/middlewares/pest-type-middleware.js
 
-const { body } = require("express-validator");
+// Core module imports
+const { body, param } = require("express-validator");
+
+// Custom module imports
 const { toArrayOfVals } = require("../../utils/common");
 const { PEST_TYPE } = require("../../constants/pest-constant");
 
-const createPestTypeValidator = [
+let createPestTypeValidator = [
     // Required field(s)
     body("name")
         .notEmpty().withMessage("name is a required field").bail()
         .isString().withMessage("name must be a string").bail()
         .trim().toLowerCase()
-        .isIn(toArrayOfVals(PEST_TYPE)).withMessage(`name must be one of ${toArrayOfVals(PEST_TYPE, true)}`),
+        .isIn(toArrayOfVals(PEST_TYPE)).withMessage(`name must be one of ${toArrayOfVals(PEST_TYPE, true)}`)
+];
 
-    // Optional field(s)
+const idValidator = [
+    param("pest_type_id")
+        .trim()
+        .notEmpty().withMessage("pest_type_id is required as path param").bail()
+        .isUUID().withMessage("pest_type_id should be a valid UUID")
+];
+
+const updatePestTypeDetailsValidator = [
     body("description")
         .optional()
         .isString().withMessage("description must be a string").bail()
         .trim()
 ];
 
+createPestTypeValidator = [ ...createPestTypeValidator, ...updatePestTypeDetailsValidator ];
+
 const pestTypeValidator = () => {
     return {
         create: createPestTypeValidator,
+        id: idValidator,
+        patchOne: updatePestTypeDetailsValidator, 
     };
 };
 

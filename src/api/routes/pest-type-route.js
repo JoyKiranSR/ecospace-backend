8ce74315f2c322565edf30aedf3af9b68cb2339c
Joy Kiran S R <srjoykiran@gmail.com>
2025-08-10 05:14:35 +0530

    Added patch endpoint route to handle update details

--- src/api/routes/pest-type-route.js
+++ src/api/routes/pest-type-route.js
@@ -1,10 +1,14 @@
 // src/api/routes/pest-type-route.js
 
+// Core module imports
 const { Router } = require("express");
+
+// Custom module imports
 const { pestTypeController } = require("../controllers/pest-type-controller");
 const { pestTypeValidator } = require("../middlewares/pest-type-middleware");
 const { validationErrorHandler } = require("../middlewares/error-middleware");
 
+// Initialize the router
 const route = Router();
 const validator = pestTypeValidator(); // Creates a validator to handle pest type
 const controller = pestTypeController(); // Creates a controller to handle routes
@@ -12,6 +16,7 @@ const controller = pestTypeController(); // Creates a controller to handle route
 // Add route endpoints
 route.post("/",  validator.create, validationErrorHandler, controller.create);
 route.get("/", controller.fetchAll);
+route.patch("/:pest_type_id", validator.id, validator.patchOne, validationErrorHandler, controller.patchUpdate);
 
 // Export routes to map to application
 module.exports = route;

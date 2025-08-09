9aadc487c78d11f28f1bf2b5f4d619c7a3cd4b13
Joy Kiran S R <srjoykiran@gmail.com>
2025-08-10 03:22:46 +0530

    Added model for pest type

---
+++ src/db/models/PestType.js
@@ -0,0 +1,37 @@
+// src/db/models/PestType.js
+
+// Core module imports
+const { DataTypes } = require("sequelize");
+
+// Custom module imports
+const sequelize = require("../../db/index");
+const { PEST_TYPE } = require("../../constants/pest-constant");
+
+const PestType = sequelize.define("PestType", {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.ENUM(Object.values(PEST_TYPE)),
+        allowNull: false,
+        unique: true,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    }
+}, {
+    tableName: "pest_types", // Custom table name
+    timestamps: true, // Adds createdAt and updatedAt
+    underscored: true, // Snakecasing conversion
+});
+
+// Synchronize the model in DB
+sequelize.sync()
+    .then(() => console.log("PestType model synchronized with the database"))
+    .catch(error => console.error("Error synchronizing PestType model:", error));
+
+// Export the PestType model
+module.exports = PestType;

9466cac14dcebeb894ada346f31da4b52b19506a
Joy Kiran S R <srjoykiran@gmail.com>
2025-08-10 04:19:29 +0530

    Added post route

---
+++ src/api/routes/pest-type-route.js
@@ -0,0 +1,16 @@
+// src/api/routes/pest-type-route.js
+
+const { Router } = require("express");
+const { pestTypeController } = require("../controllers/pest-type-controller");
+const { pestTypeValidator } = require("../middlewares/pest-type-middleware");
+const { validationErrorHandler } = require("../middlewares/error-middleware");
+
+const route = Router();
+const validator = pestTypeValidator(); // Creates a validator to handle pest type
+const controller = pestTypeController(); // Creates a controller to handle routes
+
+// Add route endpoints
+route.post("/",  validator.create, validationErrorHandler, controller.create);
+
+// Export routes to map to application
+module.exports = route;
